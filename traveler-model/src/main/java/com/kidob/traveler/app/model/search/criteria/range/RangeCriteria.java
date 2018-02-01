71a64cea473c9bb00c25e6bb77353e824097a9d0
Bogdan Maateratskyy <bogdan.materatskyy@gmail.com>
2018-02-01 13:02:54 +0200

    Changed check for parameters in the constractor

--- traveler-model/src/main/java/com/kidob/traveler/app/model/search/criteria/range/RangeCriteria.java
+++ traveler-model/src/main/java/com/kidob/traveler/app/model/search/criteria/range/RangeCriteria.java
@@ -1,6 +1,7 @@
 package com.kidob.traveler.app.model.search.criteria.range;
 
 import com.kidob.traveler.app.infra.exception.flow.InvalidParameterException;
+import com.kidob.traveler.app.infra.util.Checks;
 
 /**
  * Pagination parameters for data retrieval operations
@@ -22,12 +23,9 @@ public class RangeCriteria {
 	private final int rowCount;
 
 	public RangeCriteria(final int page, final int rowCount) {
-		if (page < 0) {
-			throw new InvalidParameterException("Incorrect page index: " + page);
-		}
-		if (rowCount < 0) {
-			throw new InvalidParameterException("Incorrect row count: " + rowCount);
-		}
+		Checks.checkParameter(page >= 0, "Incorrect page index: " + page);
+		Checks.checkParameter(rowCount >= 0, "Incorrect row count: " + rowCount);
+
 		this.page = page;
 		this.rowCount = rowCount;
 	}
