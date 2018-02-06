fc927a3c088998d9ff7c9785a73ed67884085842
Bogdan Maateratskyy <bogdan.materatskyy@gmail.com>
2018-02-06 14:53:22 +0200

    Created REST service configuration for Jersey

---
+++ traveler-service/src/main/java/org/kidob/traveler/app/rest/service/config/JerseyConfig.java
@@ -0,0 +1,20 @@
+package org.kidob.traveler.app.rest.service.config;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("api")
+
+/**
+ * REST web-service configuration for Jersey
+ * 
+ * @author Bogdan
+ *
+ */
+public class JerseyConfig extends ResourceConfig {
+
+	public JerseyConfig() {
+		packages("com.kidob.travel.app.rest");
+	}
+}
