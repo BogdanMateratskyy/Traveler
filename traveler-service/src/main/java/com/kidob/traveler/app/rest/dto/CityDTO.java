c3707546a589c35b77fd9791be49cab8cd08f221
BogdanMateratskyy <bogdan.materatskyy@gmail.com>
2018-02-15 14:36:36 +0200

    Created DTO classes

---
+++ traveler-service/src/main/java/com/kidob/traveler/app/rest/dto/CityDTO.java
@@ -0,0 +1,50 @@
+package com.kidob.traveler.app.rest.dto;
+
+import com.kidob.traveler.app.model.entity.geography.City;
+import com.kidob.traveler.app.rest.dto.base.BaseDTO;
+
+/**
+ * Holds city state for the client-server communication
+ * 
+ * @author Bogdan
+ *
+ */
+public class CityDTO extends BaseDTO<City> {
+
+	private String name;
+
+	/**
+	 * Name of the district where city is placed
+	 */
+	private String district;
+
+	/**
+	 * Name of the region where district is located. 
+	 * Region is top-level area in the country
+	 */
+	private String region;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+}
