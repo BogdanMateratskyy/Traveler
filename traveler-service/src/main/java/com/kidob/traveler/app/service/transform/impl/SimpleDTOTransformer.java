ab6e5ae2fff4eb636a71a5254dbeb63b8e03eae3
BogdanMateratskyy <bogdan.materatskyy@gmail.com>
2018-02-23 15:20:56 +0200

    Added logging libraries

--- traveler-service/src/main/java/com/kidob/traveler/app/service/transform/impl/SimpleDTOTransformer.java
+++ traveler-service/src/main/java/com/kidob/traveler/app/service/transform/impl/SimpleDTOTransformer.java
@@ -1,5 +1,10 @@
 package com.kidob.traveler.app.service.transform.impl;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.kidob.traveler.app.infra.util.Checks;
 import com.kidob.traveler.app.infra.util.ReflectionUtil;
 import com.kidob.traveler.app.model.entity.base.AbstractEntity;
@@ -14,6 +19,8 @@ import com.kidob.traveler.app.service.transform.Transformer;
  */
 public class SimpleDTOTransformer implements Transformer {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDTOTransformer.class);
+
 	@Override
 	public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(final T entity, final Class<P> clz) {
 		checkParams(entity, clz);
@@ -22,6 +29,11 @@ public class SimpleDTOTransformer implements Transformer {
 		// Now just copy all the similar fields
 		ReflectionUtil.copyFields(entity, dto, ReflectionUtil.findSimilarFields(entity.getClass(), clz));
 		dto.transform(entity);
+
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("SimpleDTOTransformer.transform: {} DTO object",
+					ReflectionToStringBuilder.toString(dto, ToStringStyle.SHORT_PREFIX_STYLE));
+		}
 
 		return dto;
 	}
@@ -39,6 +51,11 @@ public class SimpleDTOTransformer implements Transformer {
 
 		ReflectionUtil.copyFields(dto, entity, ReflectionUtil.findSimilarFields(dto.getClass(), clz));
 		dto.untransform(entity);
+
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("SimpleDTOTransformer.transform: {} entity",
+					ReflectionToStringBuilder.toString(entity, ToStringStyle.SHORT_PREFIX_STYLE));
+		}
 
 		return entity;
 	}
